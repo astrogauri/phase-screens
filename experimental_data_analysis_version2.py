04217b70a216e35b3d22cfe6408e6b09eb9f58de
clovis <Clovis.Faure@obspm.fr>
2025-11-21 10:27:28 +0100

    Modification for the experimental

--- experimental_data_analysis_version2.py
+++ experimental_data_analysis_version2.py
@@ -6,30 +6,32 @@ def pli(image):
     return plt.imshow(image.T, origin="lower")
 
 # Load experimental data
-path = "/Users/gauripadalkar/Desktop/phase-screens/bench_data/"
+path = "/Users/faure/OneDrive/Bureau/git_kraken/phase-screens/bench_data/"
 f_list = ["bg_10000.fits", "no-phase_screen_10000.fits", "phase_screen_10000.fits"]
 
 def mean_frames(file_path):
     data = pf.getdata(file_path)
     return np.mean(data, axis=0)
 
-# Background
+# Background, only 'parasite' light sources
 bg = mean_frames(path + f_list[0])
 
-# No Phase Screen
+# No Phase Screen, bg + source, no screen phase
 no_phase = mean_frames(path + f_list[1]) - bg
 
-# With Phase Screen
+# With Phase Screen, bg + source, with phase screen
 phase_screen = mean_frames(path + f_list[2]) - bg
 
 # Coordinates to determine radius of C circle
 x = 315.4 - 93.8
 y = 120.6 - 100.4
-D = np.sqrt(x**2 + y**2)
-r = D/2 - 20
 
-nx, ny = 640, 480
-x0, y0 = 200, 115
+D = np.sqrt(x**2 + y**2)
+r = D/2 - 20 # We reduce r to get interesting signal only 
+
+nx, ny = 640, 480 #Image size
+x0, y0 = 200, 115 #Center of the considered circle
+
 
 
 def meshgrid(nx, ny, center_x, center_y, shift_x, shift_y):
@@ -44,11 +46,13 @@ def recenter(img, old_cx, old_cy, new_cx, new_cy):
     shift_y = int(new_cy - old_cy)
     return np.roll(np.roll(img, shift_x, axis=0), shift_y, axis=1)
 
+#Extracting the four pupillas 
 # C
 xx_c, yy_c = meshgrid(nx, ny, x0, y0, 0, 0)
 mask_c = np.sqrt(xx_c**2 + yy_c**2) < r
 
-just_new_c = np.zeros((nx, ny))
+#just_new_c = np.zeros((nx, ny))
+just_new_c = np.ones((nx, ny))
 just_new_c[mask_c] = phase_screen[mask_c]
 
 # D
@@ -58,7 +62,7 @@ shift_pix_y_d = 109 - 109
 xx_d, yy_d = meshgrid(nx, ny, x0, y0, shift_pix_x_d, shift_pix_y_d)
 mask_d = np.sqrt(xx_d**2 + yy_d**2) < r
 
-just_new_d = np.zeros((nx, ny))
+just_new_d = np.ones((nx, ny))
 just_new_d[mask_d] = phase_screen[mask_d]
 
 # A
@@ -68,7 +72,7 @@ shift_pix_y_a = 373.6 - 109.4
 xx_a, yy_a = meshgrid(nx, ny, x0, y0, shift_pix_x_a, shift_pix_y_a)
 mask_a = np.sqrt(xx_a**2 + yy_a**2) < r
 
-just_new_a = np.zeros((nx, ny))
+just_new_a = np.ones((nx, ny))
 just_new_a[mask_a] = phase_screen[mask_a]
 
 
@@ -79,11 +83,11 @@ shift_pix_y_b = 373.6 - 109.4
 xx_b, yy_b = meshgrid(nx, ny, x0, y0, shift_pix_x_b, shift_pix_y_b)
 mask_b = np.sqrt(xx_b**2 + yy_b**2) < r
 
-just_new_b = np.zeros((nx, ny))
+just_new_b = np.ones((nx, ny))
 just_new_b[mask_b] = phase_screen[mask_b]
 
-# Recentering all to C center coordinate to be able to calculate gradients
-center_ref_x = x0              
+# Recentering all pupilla center coordinate to be able to calculate gradients
+center_ref_x = x0         
 center_ref_y = y0
 
 center_c_x, center_c_y = x0, y0
@@ -104,11 +108,62 @@ grady = centered_a + centered_b - (centered_c + centered_d)
 
 normalization = centered_a + centered_b + centered_c + centered_d
 
+gradx = gradx / normalization
+grady = grady / normalization
+
 pli(normalization)
 plt.show()
 
 pli(gradx)
+plt.title('Grad x phase')
 plt.show()
 
 pli(grady)
+plt.title('Grad y phase')
 plt.show()
+
+
+
+#Computing the autocorrelation of gradx and grad y :
+
+def laplacian_dphi(r):
+    return (25/9) * r**(-1/3)
+
+def phase_gradient_vector_cov(gradx, grady):
+    n = 100
+    
+    # Gradient components
+    phase_dx =gradx # dphi/dx
+    phase_dy = grady # dphi/dy
+
+    # Extended phase screen
+    ext_phase_dx = np.zeros((2*n,2*n))
+    ext_phase_dy = np.zeros((2*n,2*n))
+    ext_phase_dx[0:n,0:n] = phase_dx
+    ext_phase_dy[0:n,0:n] = phase_dy
+
+    # Autocorrelations
+    auto_dx = (np.fft.ifft2(np.abs(np.fft.fft2(ext_phase_dx))**2)).real
+    auto_dy = (np.fft.ifft2(np.abs(np.fft.fft2(ext_phase_dy))**2)).real
+    # Shift zero frequency component to center
+    auto_dx = np.fft.fftshift(auto_dx)
+    auto_dy = np.fft.fftshift(auto_dy)
+    # Crop central region
+    auto_dx = auto_dx[n//2:n//2+n, n//2:n//2+n]
+    auto_dy = auto_dy[n//2:n//2+n, n//2:n//2+n]
+
+    # Normalization
+    support = np.zeros((2*n,2*n))
+    support[0:n,0:n] = 1
+    normalization = (np.fft.ifft2(np.abs(np.fft.fft2(support))**2)).real
+    normalization = np.fft.fftshift(normalization)
+    normalization = normalization[n//2:n//2+n, n//2:n//2+n] 
+
+    # Applying normalization
+    auto_dx_normalized = auto_dx / normalization
+    auto_dy_normalized = auto_dy / normalization  
+
+    covariance = auto_dx_normalized + auto_dy_normalized
+
+    return phase_dx, phase_dy, auto_dx_normalized, auto_dy_normalized, covariance
+
